316b584ccc5e22c71111b19d9526820654e28194
gadelhap <p186985@dac.unicamp.br>
2021-04-09 17:47:30 -0300

    implementei os metodos apresenta e passo

--- lab03/src/mc322/lab03/Animacao.java
+++ lab03/src/mc322/lab03/Animacao.java
@@ -4,6 +4,11 @@ public class Animacao{
     AquarioLombriga aquarioLombriga;
     String passos;
     int passo;
+    /**
+     * animacao: string representando estado inicial da lombriga no aquário e
+     * sequência de passo da animação.
+     * Inicializa uma animação. 
+     */
     Animacao(String animacao){
         int tamAquario, tamLombriga, posicao;
         tamAquario = animacao.substring(0, 2);
@@ -12,5 +17,33 @@ public class Animacao{
         this.aquarioLombriga = new AquarioLombriga(tamAquario, tamLombriga, posicao);
         this.passos = animacao.substring(6);
         this.passo = 0;
+    }
+    /**
+     * Imprime na tela o estado atual da lombriga no aquário da animação.
+     */
+    void apresenta(){
+        this.aquarioLombriga.apresenta();
+    }
+    /**
+     * Atualiza a animação realizando o próximo passo.
+     */
+    void passo(){
+        try{
+            switch(this.passos[this.passo]){
+                case 'C':
+                    this.aquarioLombriga.crescer();
+                    break;
+                case 'M':
+                    this.aquarioLombriga.mover();
+                    break;
+                case 'V':
+                    this.aquarioLombriga.virar();
+                    break;
+            }
+            this.passo++;
+        }
+        catch (StringIndexOutOfBoundsException e){
+            return;
+        }
     }
 }
