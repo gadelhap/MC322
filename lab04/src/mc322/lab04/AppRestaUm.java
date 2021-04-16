dd89dddae946b6117ccf2a6433c61e8744a26c94
gadelhap <p186985@dac.unicamp.br>
2021-04-15 23:41:30 -0300

    fizemos o AppRestaUm

---
+++ lab04/src/mc322/lab04/AppRestaUm.java
@@ -0,0 +1,22 @@
+package mc322.lab04;
+
+public class AppRestaUm{
+    String[] executaJogo(String path){
+        Tabuleiro tabuleiro = new Tabuleiro();
+        CSVReader csv = new CSVReader();
+        csv.setDataSource(path);
+        String comandos[] = csv.requestCommands();
+        String historico[] = new String[comandos.length + 1];
+        historico[0] = tabuleiro.paraString();
+        System.out.println("Tabuleiro inicial:");
+        tabuleiro.apresentar();
+        for (int i = 1; i <= comandos.length; i++){
+            tabuleiro.realizarComando(comandos[i]);
+            historico.append(tabuleiro.paraString());
+            System.out.println("Source: " + comandos[i].substring(0, 2));
+            System.out.println("Target: " + comandos[i].substring(3, 5));
+            tabuleiro.apresentar();
+        }
+        return historico;
+
+}
