5fb4f6f5b72547e797d5131ea2bb3fb06ef4b75f
André Ricardo <dedaricardo@gmail.com>
2021-04-16 07:36:03 -0300

    criacao de alguns itens do construtor

--- lab04/src/mc322/lab04/Peca.java
+++ lab04/src/mc322/lab04/Peca.java
@@ -1,5 +1,29 @@
 package mc322.lab04;
 
 public class Peca {
-    
+    String posicao; //"0" caso seja um espaco válido vazio, "!" caso seja invalido
+
+    Peca(String posicao){
+        this.posicao = posicao;
+    }
+
+    public int letraParaInteiro(char letra){
+        return (int)letra - (int)('a');
+    }
+
+    public void movimentarDireita(){
+
+    }
+    public void movimentarEsquerda(){
+
+    }
+
+    public void movimentarCima(){
+
+    }
+
+    public void movimentarBaixo(){
+
+    }
+
 }
--- lab04/src/mc322/lab04/Tabuleiro.java
+++ lab04/src/mc322/lab04/Tabuleiro.java
@@ -1,5 +1,41 @@
 package mc322.lab04;
 
 public class Tabuleiro {
-    
+    Peca[][] pecas;
+    int quantidadePecas;
+
+    public int letraParaInteiro(char letra){
+        return (int)letra - (int)('a');
+    }
+
+    public char inteiroParaLetra(int i){
+        return (char)(i + (int)('a'));
+    }
+
+    Tabuleiro(){
+        pecas = new Peca[7][7];
+        for (int i = 0; i < 7; i++){
+            for (int j = 0; j < 7; j++){
+                if ((i < 2 && j < 2) | (i < 2 && j > 4) | (i > 4 && j > 4) | (i > 4 && j < 2)){
+                    pecas[i][j] = new Peca("!");
+                } else if (i == 3 && j == 3){
+                    pecas[3][3] = new Peca("0");
+                } else {
+                    char coluna = inteiroParaLetra(j);
+                    char linha = (char)(7 - i); //!!!!!lembrar que as linhas vão de 7 a 1 (reverso)
+                    String posicao = Character.toString(coluna) + Character.toString(linha);
+                    pecas[i][j] = new Peca(posicao);
+                }
+            }
+        }
+    }
+
+    public void realizarComando(String comando){
+    }
+
+    public String converterString(); //converte o jogo de matriz para string
+
+    public void apresentar(); //apresenta com a formatacao desejada
+
+
 }
